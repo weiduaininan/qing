8f7f4beeba367f948b61ff7f85414546b7c77d9f
liunan <bj_liunan@126.com>
2021-01-31 21:01:49 +0800

    增加系统设置管理

--- app/qingadmin/controller/Config.php
+++ app/qingadmin/controller/Config.php
@@ -1,11 +1,8 @@
 <?php
 namespace app\qingadmin\controller;
+use think\facade\Db;
 
 class Config extends Base {
-	public function index() {
-		return view();
-	}
-
 	//清空runtime下的缓存
 	public function del_cache() {
 		//$path=runtime_path();//应用运行时目录runtime/qingadmin
@@ -15,4 +12,30 @@ class Config extends Base {
 		return alert('清空缓存成功', '/qingadmin/index/welcome', 6);
 	}
 
+	/**
+	 * 系统设置
+	 * @Author   liunan
+	 * @DateTime 2021-01-31T17:02:42+0800
+	 * @Email    bj_liunan@126.com
+	 * @return   [type]                   [description]
+	 */
+	public function index() {
+		$configData1 = Db::name('config')->where('config_type', 1)->select();
+		$configData2 = Db::name('config')->where('config_type', 2)->select();
+
+		if (request()->isPost()) {
+			$data = input('post.');
+			//halt($data);
+			foreach ($data as $k => $v) {
+				Db::name('config')->where('id', $k)->update(['value' => $v]);
+			}
+
+			return alert('操作成功', 'index', 6);
+		} else {
+			return view('', [
+				'configData1' => $configData1,
+				'configData2' => $configData2,
+			]);
+		}
+	}
 }
